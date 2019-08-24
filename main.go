9fb47c3ba401e5a1ed534f4b02fdf750e75a6a3d
Steven Fisher <stevef1uk@gmail.com>
2019-08-24 20:13:45 +0000

    test

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Hello world received a requesty.")
+	target := os.Getenv("TARGET")
+	if target == "" {
+		target = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", target)
+}
+
+func main() {
+	log.Print("Hello world sample started.")
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
