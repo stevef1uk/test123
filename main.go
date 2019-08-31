befc42cf22f40ddc9d8e4a221d6bd7db6db9a38b
Steven Fisher <stevef1uk@gmail.com>
2019-08-31 10:36:14 +0000

    test

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Print("Hello world received a requesty.")
+	log.Print("Hello world received a request.")
 	target := os.Getenv("TARGET")
 	if target == "" {
 		target = "World"
